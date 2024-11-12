360c904c6d0578b2005bbc5bdebdd09a8c70226e
ZhangYong <zhangy@nanjingdadao.com>
2024-11-12 10:29:30 +0800

    update proto with project

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use rand::Rng;
 
 pub mod hello {
-    tonic::include_proto!("hello");
+    tonic::include_proto!("ford.ivi.h625");
 }
 
-use hello::HelloRequest;
+use hello::FooRequest;
 
 #[derive(Default)]
 pub struct MyGreeter {}
@@ -14,20 +14,20 @@ pub struct MyGreeter {}
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 客户端测试
-    let mut client = hello::greeter_client::GreeterClient::connect("http://[::1]:50051").await?;
+    let mut client = hello::greeter_client::GreeterClient::connect("http://127.0.0.1:50051").await?;
 
     // 随机选择一个字符串出来
-    let names = ["张三", "李四", "王五"];
+    let names = ["one", "two", "three"];
 
     let mut rng = rand::thread_rng();
 
     let random_name = names[rng.gen_range(0..names.len())];
 
-    let request = tonic::Request::new(HelloRequest {
+    let request = tonic::Request::new(FooRequest {
         name: random_name.into(),
     });
 
-    let response = client.say_hello(request).await?;
+    let response = client.foo(request).await?;
 
     println!("RESPONSE={:?}", response.into_inner().message);
 
